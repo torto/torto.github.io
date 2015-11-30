ccff4cd2ec2455ccc7fac7f9a45f4f6b8ccab4fd
Guilherme Berghauser <tortoyoyo@gmail.com>
2015-11-30 14:13:22 -0200

    Ajuste final link

--- contaazul/js/directives/table-list/TableListDirective.js
+++ contaazul/js/directives/table-list/TableListDirective.js
@@ -1,7 +1,7 @@
 angular.module('conta-azul').directive('tableList', [function() {
   return {
     restrict: 'E',
-    templateUrl: '/partials/table-list/table-list.html',
+    templateUrl: 'partials/table-list/table-list.html',
     scope: {
       options: "=",
       pagination: "@",
