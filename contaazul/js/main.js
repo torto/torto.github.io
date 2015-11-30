ed4f2c789322ace0bcbb657c3baf610f554eab5b
Guilherme Berghauser <tortoyoyo@gmail.com>
2015-11-30 14:08:31 -0200

    tentando ajustar

--- contaazul/js/main.js
+++ contaazul/js/main.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('conta-azul', ['ngRoute']).config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider) {
-$locationProvider.html5Mode(true).hashPrefix('!');
+$locationProvider.html5Mode(true);
     $routeProvider.when('/', {
       templateUrl: 'partials/index.html',
       controller: 'IndexController'
